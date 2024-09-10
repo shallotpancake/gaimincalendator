298d796fdd40b10fa65ec326152f27ae352029a1
shallotpancake <shallot_pk+github@proton.me>
2024-09-10 15:10:48 -0500

    change stream schema

--- models.py
+++ models.py
@@ -1,16 +1,30 @@
 class Stream:
-    def __init__(self, title):
-        self.title = title
+    def __init__(self, service, stream_id):
+        self.service = service
+        self.stream_id = stream_id
+        self.link = self.construct_link()
+
+    def construct_link(self):
+        """
+        Constructs a stream link based on the service (platform).
+        """
+        if self.service == 'youtube':
+            return f"https://youtube.com/watch?v={self.stream_id}"
+        elif self.service == 'twitch':
+            return f"https://twitch.tv/{self.stream_id}"
+        # Add more services as needed
+        else:
+            return f"Unknown service: {self.service}"
 
     def __repr__(self):
-        return f"Stream(title='{self.title}')"
+        return self.link
 
 class Match:
     def __init__(self, team_left, team_right, tournament, streams, data_timestamp, data_tz):
         self.team_left = team_left
         self.team_right = team_right
         self.tournament = tournament
-        self.streams = streams  # Dictionary of categories to lists of Stream objects
+        self.streams = streams  
         self.data_timestamp = data_timestamp
         self.data_tz = data_tz
 
@@ -18,3 +32,6 @@ class Match:
         return (f"Match(team_left='{self.team_left}', team_right='{self.team_right}', "
                 f"tournament='{self.tournament}', streams={self.streams}, "
                 f"data_timestamp='{self.data_timestamp}', data_tz='{self.data_tz}')")
+
+if __name__ == "__main__":
+    pass
