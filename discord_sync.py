96a6ab4f4571cdfa8c4e5f25a7896e3248866d74
shallotpancake <shallot_pk+github@proton.me>
2024-10-16 22:26:12 -0500

    fix discord integration, address duplicates

--- discord_sync.py
+++ discord_sync.py
@@ -1,120 +1,148 @@
 import requests
 import json
-from datetime import datetime, timedelta
-import string
-import random
-from collections import defaultdict
+from event import Event
+from dotenv import load_dotenv
+import os
+import cache_discord
+from time import sleep
 
-DISCORD_API_URL = "https://discord.com/api/v10"
-
-class DiscordEventSync:
-    def __init__(self, bot_token, guild_id, channel_id):
-        self.bot_token = bot_token
-        self.guild_id = guild_id
-        self.channel_id = channel_id
-        self.headers = {
-            "Authorization": f"Bot {self.bot_token}",
-            "Content-Type": "application/json"
-        }
-
-    def create_unique_id(self):
-        """
-        Creates a unique identifier for a match
-        """
-        # Define the characters to choose from (letters and digits)
-        characters = string.ascii_letters + string.digits
-        # Use random.choices to generate a list of random characters, then join them into a string
-        random_string = ''.join(random.choices(characters, k=50))
-        return random_string
-
-    def search_event_by_unique_id(self, unique_id):
-        """
-        Searches for an event with a given unique identifier in extended properties.
-        """
-        response = requests.get(
-            f"{DISCORD_API_URL}/guilds/{self.guild_id}/scheduled-events",
-            headers=self.headers
-        )
-
-        response.raise_for_status()
-
-        events = response.json()
-        for event in events:
-            if unique_id in event['description']:
-                return event
-
-        return None
-    
-    def match_to_event(self, match):
-        unique_id = self.create_unique_id()
-        event_data = {
-            "name": f"{match.team_left} vs {match.team_right}",
-            "description": f"Tournament: {match.tournament}\nStreams: {', '.join([str(stream) for stream in match.streams.values()])}\n{unique_id}",
-            "scheduled_start_time": match.data_timestamp.isoformat(),
-            "scheduled_end_time": (match.data_timestamp + timedelta(hours=2)).isoformat(),
-            "privacy_level": 2,  # GUILD_ONLY
-            "entity_type": 3,  # External event
-            "entity_metadata": {
-                "location": "Online"
-            }
-        }
-        return event_data
-
-    def add_or_update_event(self, match):
-        """
-        Adds a new event or updates an existing one if found using a unique identifier.
-        """
-        print("add_or_update_event")
-        #existing_event = self.search_event_by_unique_id(unique_id) the discord API is too sensitive for this type of check. maybe I just delete all events and write new on run
-        #existing_event = None 
-        event = self.match_to_event(match)
-        # Create a new event
-        print(f"Creating new event: {event['name']}")
-        response = requests.post(
-            f"{DISCORD_API_URL}/guilds/{self.guild_id}/scheduled-events",
-            headers=self.headers,
-            data=json.dumps(event)
-        )
-        try:
-            print(response.text)
-            response.raise_for_status()
-        except:
-            pass
-
-
-    def delete_event_by_unique_id(self, unique_id):
-        """
-        Deletes an event with the specified unique ID.
-        """
-        existing_event = self.search_event_by_unique_id(unique_id)
-        if existing_event:
-            event_id = existing_event['id']
-            response = requests.delete(
-                f"{DISCORD_API_URL}/guilds/{self.guild_id}/scheduled-events/{event_id}",
-                headers=self.headers
-            )
-
-            if response.status_code == 204:
-                print(f"Deleted event: {existing_event['name']}")
-            else:
-                print(f"Failed to delete event: {response.status_code}, {response.text}")
-        else:
-            print(f"No event found with ID: {unique_id}")
-            
-if __name__ == "__main__":
-    import os
-    from dotenv import load_dotenv
-    from datetime import datetime, timedelta, timezone
-    from models import Match, Stream
+def post_event(match, uid):
+    """
+    Posts event to Discord
+    """
     load_dotenv()
-    streams = defaultdict(list)
-    streams['twitch'].append(Stream('twitch', 'The_International'))
-    match = Match("Test", "Test", "TI", streams, (datetime.now(timezone.utc) + timedelta(hours=1)), "UTC-05: 00")
-    
+    DISCORD_API_URL = "https://discord.com/api/v10"
     BOT_TOKEN = os.environ.get('DISCORD_BOT_TOKEN')
     GUILD_ID = os.environ.get('GUILD_ID')
-    CHANNEL_ID = os.environ.get('CHANNEL_ID')
-    
-    discord_sync = DiscordEventSync(BOT_TOKEN, GUILD_ID, CHANNEL_ID)
-    discord_sync.add_or_update_event(match)
+    headers = {
+        "Authorization": f"Bot {BOT_TOKEN}",
+        "Content-Type": "application/json"
+    }
+        
+    event = event_to_guild_event(match)
+    # Create a new event
+    print(f"Creating new event: {match}")
+    response = requests.post(
+        f"{DISCORD_API_URL}/guilds/{GUILD_ID}/scheduled-events",
+        headers=headers,
+        data=json.dumps(event)
+    )
+    try:
+        print(response.text)
+        response.raise_for_status()
+    except:
+        pass
+    with open("temp/event_id_cache.txt", "a") as f:
+        f.write(f"{uid}\n")
+    return response.status_code
 
+def compare_event_discord_event(event: Event, discord_event: dict)-> bool:
+    return event.uid in discord_event["description"]
+
+
+
+def event_to_guild_event(event: Event):
+    # convert Event to discord_
+    event_data = {
+        "name": event.summary,
+        "description": event.description,
+        "scheduled_start_time": event.start_time,
+        "scheduled_end_time": event.end_time,
+        "privacy_level": 2,  # GUILD_ONLY
+        "entity_type": 3,  # External event
+        "entity_metadata": {
+            "location": "Online"
+        }
+    }
+    return event_data
+    
+def get_discord_events():
+    """
+    Get events from Discord
+    """
+    load_dotenv()
+    BOT_TOKEN = os.environ.get('DISCORD_BOT_TOKEN')
+    DISCORD_API_URL = "https://discord.com/api/v10"
+    GUILD_ID = os.environ.get('GUILD_ID')
+
+    headers = {
+        "Authorization": f"Bot {BOT_TOKEN}",
+        "Content-Type": "application/json"
+    }
+
+    # Get all existing events
+    response = requests.get(
+        f"{DISCORD_API_URL}/guilds/{GUILD_ID}/scheduled-events",
+        headers=headers
+    )
+    try:
+        response.raise_for_status()
+    except:
+        print(response.text)
+        return None
+
+
+    discord_events = response.json()
+
+    return discord_events
+
+def remove_duplicates(incoming_events: list[Event]) -> list:
+    unique_events = []
+
+    with open("temp/event_id_cache.txt", "r") as f:
+        existing_events = f.read()
+
+    print(f"Starting event count: {len(incoming_events)}")
+    for event in incoming_events:
+        if event.uid in existing_events:
+            print(f"Event duplicate: {event.uid}")
+            print(existing_events)
+            continue
+        else:
+            unique_events.append(event)
+    
+    print(f"Ending event count: {len(unique_events)}")
+    return unique_events
+
+def clear_events():
+    load_dotenv()
+    BOT_TOKEN = os.environ.get('DISCORD_BOT_TOKEN')
+    DISCORD_API_URL = "https://discord.com/api/v10"
+    GUILD_ID = os.environ.get('GUILD_ID')
+
+    headers = {
+        "Authorization": f"Bot {BOT_TOKEN}",
+        "Content-Type": "application/json"
+    }
+
+    events = get_discord_events()
+    for event in events:
+        requests.delete(
+        f"{DISCORD_API_URL}/guilds/{GUILD_ID}/scheduled-events/{event["id"]}",
+        headers=headers
+        )
+        sleep(1)
+
+    sleep(10)
+    events = get_discord_events()
+
+    if events:
+        for event in events:
+            requests.delete(
+            f"{DISCORD_API_URL}/guilds/{GUILD_ID}/scheduled-events/{event["id"]}",
+            headers=headers
+            )
+            sleep(1)
+
+def DiscordSync(incoming_events: list):
+    new_events = remove_duplicates(incoming_events=incoming_events)
+
+    i = 0
+    for event in new_events:
+        if i > 0:
+            break   
+        post_event(event, event.uid)
+        i+=1
+        sleep(10)
+
+    
--- main.py
+++ main.py
@@ -3,17 +3,22 @@ from google_sync import GoogleCalendarSync
 from dotenv import load_dotenv
 import os
 import env_setup
-from discord_sync import DiscordEventSync
+from discord_sync import DiscordSync
 import time
+from event import matches_to_event
+
+do_discordsync = True
 
 env_setup.load_environment()
-scraper = Scrape(os.environ.get('URL'))
-match_entries = scraper.parse_match_entries()
-print(f"scraper.py - Match entries found: {len(match_entries)}")
+scraper = Scrape()
+
+events = scraper.events
 with open('matches.json', 'w') as f:
-    f.write(str(match_entries))
+    f.write(str(events))
 
 # Set up Google Calendar sync
+
+
 load_dotenv() # load ID from .env
 GOOGLE_CALENDAR_ID = os.environ.get('CALENDAR_ID')
 google_sync = GoogleCalendarSync()
@@ -22,21 +27,14 @@ google_sync = GoogleCalendarSync()
 BOT_TOKEN = os.environ.get('DISCORD_BOT_TOKEN')
 GUILD_ID = os.environ.get('GUILD_ID')
 CHANNEL_ID = os.environ.get('CHANNEL_ID')
-discord_sync = DiscordEventSync(BOT_TOKEN, GUILD_ID, CHANNEL_ID)
 
 print("Syncing match entries to Google Calendar:")
-for index, match in enumerate(match_entries):
-    if index > 5: # limit this to 5 events, too noisy
-        break
-    google_sync.add_or_update_event(GOOGLE_CALENDAR_ID, match)
+for match in events:
+    #google_sync.add_or_update_event(GOOGLE_CALENDAR_ID, match)
+    ...
     
 print("Google calendar sync complete.")
-    
-print("Syncing match entries to Discord Guild Events:")
-for index, match in enumerate(match_entries):
-    if index > 5: # limit this to 5 events, discord API is annoying
-        break
-    discord_sync.add_or_update_event(match) #this is completely broken, maybe
-    time.sleep(5) # discord keeps rate limiting me reeeeeeeee
+
+DiscordSync(events)
     
 print("Discord events sync complete.")
