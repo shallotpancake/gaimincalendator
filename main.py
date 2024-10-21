c973a29ca0d48f845c51f44e61b2e2f8748d5766
shallotpancake <shallot_pk+github@proton.me>
2024-10-21 14:18:02 -0500

    comment gcal, add path checking

--- main.py
+++ main.py
@@ -6,6 +6,21 @@ import env_setup
 from discord_sync import DiscordSync
 import time
 from event import matches_to_event
+from pathlib import Path
+
+root = os.getcwd()
+
+paths = ['temp/',]
+
+for path in paths:
+    p = Path(root).joinpath(path)
+    p.mkdir(exist_ok=True)
+
+files = ['temp/event_id_cache.txt',]
+for file in files:
+    f = Path(root).joinpath(file)
+    f.touch(exist_ok=True)
+
 
 do_discordsync = True
 
@@ -20,8 +35,8 @@ with open('matches.json', 'w') as f:
 
 
 load_dotenv() # load ID from .env
-GOOGLE_CALENDAR_ID = os.environ.get('CALENDAR_ID')
-google_sync = GoogleCalendarSync()
+#GOOGLE_CALENDAR_ID = os.environ.get('CALENDAR_ID')
+#google_sync = GoogleCalendarSync()
 
 # Set up Discord Event sync
 BOT_TOKEN = os.environ.get('DISCORD_BOT_TOKEN')
